cda3aec923ada9d0c78527a90912015e29e9f0a8
marina <mara.vetis8@gmail.com>
2022-08-16 19:42:48 -0700

    first commit

---
+++ BinarySearchTest.java
@@ -0,0 +1,17 @@
+package BinarySearch;
+
+public class BinarySearchTest {
+    public static void main(String[] args){
+        BinarySearch bS = new BinarySearch();
+    // BinarySearch newSearch = new BinarySearch();
+    // BinarySearch testSearch = new BinarySearch();
+    
+    
+        // System.out.println(BinarySearch.binarySearch(test, 5));
+        // System.out.println(BinarySearch.binarySearch(test, 1));
+        // System.out.println(BinarySearch.binarySearch(test, 8));
+        // System.out.println(BinarySearch.binarySearch(test, 4));
+    }
+
+   
+}
